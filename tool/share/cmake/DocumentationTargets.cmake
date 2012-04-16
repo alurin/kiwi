5900d902e5e952e8fce137ab565807746b251407
Vasiliy Sheredeko <piphon@gmail.com>
2012-04-16 12:13:26 +0600

    Update docs target

--- tool/share/cmake/DocumentationTargets.cmake
+++ tool/share/cmake/DocumentationTargets.cmake
@@ -6,30 +6,30 @@
 
 FIND_PACKAGE (Doxygen)
 IF (DOXYGEN_FOUND)
-    IF (EXISTS ${PROJECT_SOURCE_DIR}/Doxyfile)
+    IF (EXISTS ${CMAKE_BINARY_DIR}/Doxyfile)
         ADD_CUSTOM_TARGET(
             doxygen
-            ${DOXYGEN_EXECUTABLE} Doxyfile
+            ${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/Doxyfile
             WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
             COMMENT "Generating doxygen project documentation." VERBATIM
         )
         ADD_CUSTOM_TARGET(
             documentation
-            ${DOXYGEN_EXECUTABLE} Doxyfile
+            ${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/Doxyfile
             WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
             COMMENT "Generating doxygen project documentation." VERBATIM
         )
         ADD_CUSTOM_TARGET(
             docs
-            ${DOXYGEN_EXECUTABLE} Doxyfile
+            ${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/Doxyfile
             WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
             COMMENT "Generating doxygen project documentation." VERBATIM
         )
-    ELSE (EXISTS ${PROJECT_SOURCE_DIR}/Doxyfile)
+    ELSE (EXISTS ${CMAKE_BINARY_DIR}/Doxyfile)
         ADD_CUSTOM_TARGET(doxygen COMMENT "Doxyfile not found. Please generate a doxygen configuration file to use this target." VERBATIM)
         ADD_CUSTOM_TARGET(documentation COMMENT "Doxyfile not found. Please generate a doxygen configuration file to use this target." VERBATIM)
         ADD_CUSTOM_TARGET(docs COMMENT "Doxyfile not found. Please generate a doxygen configuration file to use this target." VERBATIM)
-    ENDIF (EXISTS ${PROJECT_SOURCE_DIR}/Doxyfile)
+    ENDIF (EXISTS ${CMAKE_BINARY_DIR}/Doxyfile)
 ELSE (DOXYGEN_FOUND)
     ADD_CUSTOM_TARGET(doxygen COMMENT "Doxygen not found. Please install doxygen to use this target." VERBATIM)
     ADD_CUSTOM_TARGET(documentation COMMENT "Doxygen not found. Please install doxygen to use this target." VERBATIM)
