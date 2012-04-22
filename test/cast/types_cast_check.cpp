2637344ca3812d0de40d5db32720150216be7e84
Vasiliy Sheredeko <piphon@gmail.com>
2012-04-22 22:56:54 +0600

    Merge testes

--- test/cast/types_cast_check.cpp
+++ test/cast/types_cast_check.cpp
@@ -2,35 +2,52 @@
 #include "kiwi/Context.hpp"
 #include "kiwi/Module.hpp"
 #include "kiwi/DerivedTypes.hpp"
+#include "kiwi/Members.hpp"
 #include "kiwi/Support/Cast.hpp"
+#include <vector>
 
 using namespace kiwi;
 
-template<typename T>
-class type_checker {
+template<typename T, typename R>
+class cast_checker {
 public:
+    typedef boost::shared_ptr<R> RootRef;
     typedef boost::shared_ptr<T> ConcreteRef;
 
     /// check type
     static void check(const ConcreteRef& instType) {
-        TypeRef anyType = instType;
+        RootRef anyType = instType;
 
-        CHECK(classof<T>(instType));
-        CHECK(classof<T>(anyType));
-        CHECK_EQUAL(instType, dyn_cast<T>(anyType));
+        CHECK(classof<T>(instType));                 // check current class
+        CHECK(classof<T>(anyType));                  // check root class
+        CHECK_EQUAL(instType, dyn_cast<T>(anyType)); // check dyn_cast
     }
 };
 
-TEST(types_cast_check) // Declares a test named "sanity_check"
+TEST(types_cast_check) // Declares a test named "types_cast_check"
 {
     ContextRef context = Context::create();
 
-    type_checker<VoidType>::check(VoidType::get(context));
-    type_checker<BoolType>::check(BoolType::get(context));
-    type_checker<CharType>::check(CharType::get(context));
-    type_checker<IntType>::check(IntType::get32(context));
-    type_checker<StringType>::check(StringType::get(context));
+    cast_checker<VoidType, Type>::check(VoidType::get(context));
+    cast_checker<BoolType, Type>::check(BoolType::get(context));
+    cast_checker<CharType, Type>::check(CharType::get(context));
+    cast_checker<IntType, Type>::check(IntType::get32(context));
+    cast_checker<StringType, Type>::check(StringType::get(context));
 
     ModuleRef  module  = Module::create("script", context);
-    type_checker<ObjectType>::check(ObjectType::create(module));
+    cast_checker<ObjectType, Type>::check(ObjectType::create(module));
+}
+
+TEST(members_cast_check) // Declares a test named "members_cast_check"
+{
+    ContextRef context = Context::create();
+    ModuleRef  module  = Module::create("name", context);
+    ObjectTy   type    = ObjectType::create(module);
+
+    std::vector<ArgumentRef> args;
+
+    cast_checker<Field, Member>::check(type->add("field", type));
+    cast_checker<Method, Member>::check(type->add("method", type, args));
+    cast_checker<UnaryOperator, Member>::check(type->add(Member::NOT, type, 0));
+    cast_checker<BinaryOperator, Member>::check(type->add(Member::ADD, type, type, 0));
 }
